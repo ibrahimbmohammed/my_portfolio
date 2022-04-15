257d3ab03f99e3c92bbd2d2c0fa899344a4e589d
user <user@users-MacBook-Pro.local>
2022-04-15 05:51:32 +0100

    project set up

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'plugin:react/jsx-runtime'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', 'ts', 'tsx'] }],
+        'import/no-extraneous-dependencies': [
+            'error',
+            {
+                devDependencies: ['**/*.stories.*', '**/.storybook/**/*.*'],
+                peerDependencies: true,
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: ['pages/**/*.tsx', 'stories/**/*.tsx'],
+            rules: {
+                'react/jsx-props-no-spreading': 'off',
+                'react/function-component-definition': 'off',
+                'import/no-unresolved': 'off',
+                'import/extensions': 'off',
+                'react/require-default-props': 'off',
+            },
+        },
+    ],
+    settings: {
+        react: {
+            version: 'latest',
+        },
+    },
+};
